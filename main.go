14d47f69c009b3e9d2abc5629d247db7e2a13cac
Ovidio R. Calvet <calveto@gmail.com>
2018-09-25 00:02:36 -0400

    Add wait group

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	ch := make(chan bool, 5)
+	ch := make(chan bool, 100)
+	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		ch <- true
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			printAndWait()
 			<-ch
 		}()
 	}
+	close(ch)
+	wg.Wait()
 }
 
 func printAndWait() {
